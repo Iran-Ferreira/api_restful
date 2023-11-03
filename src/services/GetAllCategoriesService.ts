dfbfe60080e21b3dadd3572dcafdf3e50473f3e8
Iran-Ferreira <iranferreira712@gmail.com>
2023-11-03 18:57:19 -0300

    feat: list category

---
+++ src/services/GetAllCategoriesService.ts
@@ -0,0 +1,14 @@
+import { PostgresDataSource } from "../../db_config";
+import { Category } from "../entities/Category";
+
+
+export class GetAllCategoriesService {
+    
+    async execute(){
+        const repo = PostgresDataSource.getRepository(Category)
+
+        const categories = await repo.find()
+
+        return categories 
+    }
+}
