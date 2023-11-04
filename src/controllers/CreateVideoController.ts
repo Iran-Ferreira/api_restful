108d0497672a04e96622d4c56354574ca3a22db0
Iran-Ferreira <iranferreira712@gmail.com>
2023-11-03 21:11:55 -0300

    feat: Create videos

---
+++ src/controllers/CreateVideoController.ts
@@ -0,0 +1,24 @@
+import { Request, Response} from "express"
+import { CreateVideoService } from "../services/CreateVideoService"
+
+export class CreateVideoController {
+    async handle(request: Request, response: Response) {
+        const { id, name, description, category_id, duration} = request.body
+
+        const service = new CreateVideoService()
+
+        const result = await service.execute({
+            name,
+            description,
+            category_id,
+            duration,
+            id
+        })
+
+        if(result instanceof Error) {
+            return response.status(400).json(result.message)
+        }
+
+        return response.json(result)
+    }
+}
