69bb02ee9fb5f6e4da6e059106bde00a563daad5
Iran-Ferreira <iranferreira712@gmail.com>
2023-11-06 11:47:02 -0300

    feat: delete video

---
+++ src/services/VideoService/DeleteVideoService.ts
@@ -0,0 +1,14 @@
+import { PostgresDataSource } from "../../../db_config";  
+import { Video } from "../../entities/Videos";
+
+export class DeleteVideoService {
+    async execute(id: string) {
+        const repo = PostgresDataSource.getRepository(Video)
+        
+        if (!(await repo.findOne({ where: { id } }))) {
+            return new Error("Video does not exists!")
+        }
+
+        await repo.delete(id)
+    }
+}
