d206a57b932c0c190d60a12e770286cb41610b49
Iran-Ferreira <iranferreira712@gmail.com>
2023-11-06 11:47:44 -0300

    fix: caminho relativo

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { CreateCategoryController } from "./controllers/CreateCategoryController";
-import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
-import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
-import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
-import { CreateVideoController } from "./controllers/CreateVideoController";
-import { GetAllVideosController } from "./controllers/GetAllVideosController";
+import { CreateCategoryController } from "./controllers/CategoryController/CreateCategoryController";
+import { GetAllCategoriesController } from "./controllers/CategoryController/GetAllCategoriesController";
+import { DeleteCategoryController } from "./controllers/CategoryController/DeleteCategoryController";
+import { UpdateCategoryController } from "./controllers/CategoryController/UpdateCategoryController";
+import { CreateVideoController } from "./controllers/VideoController/CreateVideoController";
+import { GetAllVideosController } from "./controllers/VideoController/GetAllVideosController";
 
 const routes = Router()
 
