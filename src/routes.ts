c5254340d46f4a0c691641ef28f767ac03e7b6f2
Iran-Ferreira <iranferreira712@gmail.com>
2023-11-13 08:25:11 -0300

    feat: route update user

--- src/routes.ts
+++ src/routes.ts
@@ -11,22 +11,27 @@ import { CreateUserController } from "./controllers/UserController/CreateUserCon
 import { GetAllUsersController } from "./controllers/UserController/GetAllUsersController";
 import { DeleteUserController } from "./controllers/UserController/DeleteUserController";
 import { LoginUserController } from "./controllers/UserController/LoginUserController";
+import { UpdateUserController } from "./controllers/UserController/UpdateUserController";
 
 const routes = Router()
 
+// Rotas de Categories
 routes.post("/categories", new CreateCategoryController().handle)
 routes.get("/categories", new GetAllCategoriesController().handle)
 routes.delete("/categories/:id", new DeleteCategoryController().handle)
 routes.put("/categories/:id", new UpdateCategoryController().handle)
 
+// Rotas de Videos
 routes.post("/videos", new CreateVideoController().handle)
 routes.get("/videos", new GetAllVideosController().handle)
 routes.delete("/videos/:id", new DeleteVideoController().handle)
 routes.put("/videos/:id", new UpdateVideoController().handle)
 
+// Rotas de Users
 routes.post("/users", new CreateUserController().handle)
 routes.post("/login", new LoginUserController().handle)
 routes.get("/users", new GetAllUsersController().handle)
 routes.delete("/users/:id", new DeleteUserController().handle)
+routes.put("/users/:id", new UpdateUserController().handle)
 
 export { routes }
