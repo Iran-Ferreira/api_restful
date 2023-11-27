894b0f5697a437bd99aec483da35100448bf2c54
Iran-Ferreira <iranferreira712@gmail.com>
2023-11-26 21:13:12 -0300

    fix

--- src/services/VideoService/CreateVideoService.ts
+++ src/services/VideoService/CreateVideoService.ts
@@ -14,15 +14,22 @@ export class CreateVideoService {
     async execute({ id, name, description, duration, category_id}: VideoRequest): Promise<Error | Video> {
         const repo = PostgresDataSource.getRepository(Video)
         const repocategory = PostgresDataSource.getRepository(Category)
+        try{
+            // Se eu colocar uma categoria que não existe no filme vai retornar um erro. 
+            if(!(await repocategory.findOne({ where: { id } }))){
+                return new Error("Category does not exists!")
+            }
 
-        // Se eu colocar uma categoria que não existe no filme vai retornar um erro. 
-        if(!(await repocategory.findOne({ where: { id } }))){
-            return new Error("Category does not exists!")
+            const video = repo.create({ name, description, duration, category_id})
+            
+            await repo.save(video)
+            return video
+            
+        }catch(error){
+            if(error.code === "23502"){
+                return new Error("null value in column name of relation videos violates not-null constraint")
+            }
         }
-
-        const video = repo.create({ name, description, duration, category_id})
-        
-        await repo.save(video)
-        return video 
+    
     }
 }
