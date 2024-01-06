09af5282fe319ecbe8760619b79c666c6bd73fe3
Iran-Ferreira <iranferreira712@gmail.com>
2024-01-06 14:55:24 -0300

    fix

--- src/modules/user/service/UpdateUserService.ts
+++ src/modules/user/service/UpdateUserService.ts
@@ -1,6 +1,5 @@
 import { UserRepository } from "../repositories/user.repository";
 
-
 export class UpdateUserService {
     constructor(private readonly userRepository: UserRepository) {}
     async execute (id: string, email: string, password: string): Promise<void>{
