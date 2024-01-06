1d884d8853b2d7af2c6e2f31a3297c9dca3e770f
Iran-Ferreira <iranferreira712@gmail.com>
2024-01-06 08:36:45 -0300

    feat: juntando repository no codigo

--- src/modules/user/controller/CreateUserController.ts
+++ src/modules/user/controller/CreateUserController.ts
@@ -2,19 +2,12 @@ import { Request, Response } from 'express';
 import { CreateUserService } from '../service/CreateUserService';
 
 export class CreateUserController {
-    async handle(request: Request, response: Response){
+    constructor(private readonly service: CreateUserService) {}
+    async handle(request: Request, response: Response): Promise<Response>{
         const { email, password } = request.body
 
-        const service = new CreateUserService()
-
-        // Execute o serviço de user para criar user
-        const result =  await service.execute({ email, password})
-
-        // Se ocorrer um erro, verificar se é uma instância de Error
-        if(result instanceof Error){
-            return response.status(400).json(result.message)
-        }
+        await this.service.execute(email, password)
         
-        return response.json(result)
+        return response.json({ message: "Usuário criado com sucesso" })
     }
 }
--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -1,10 +1,16 @@
+import { TypeormUserRepository } from "./repositories/implementations/typeorm-user.repository";
 import { CreateUserController } from "./controller/CreateUserController";
+import { CreateUserService } from "./service/CreateUserService";
 import { GetAllUsersController } from "./controller/GetAllUsersController";
 import { DeleteUserController } from "./controller/DeleteUserController";
 import { UpdateUserController } from "./controller/UpdateUserController";
 import { LoginUserController } from "./controller/LoginUserController";
 
-const createController = new CreateUserController()
+const repository = new TypeormUserRepository()
+
+const createService = new CreateUserService(repository)
+const createController = new CreateUserController(createService)
+
 const getAllController = new GetAllUsersController()
 const deleteController = new DeleteUserController()
 const updateController = new UpdateUserController()
